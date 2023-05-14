4f4f5265f2d716334f73f706398eb4bbdf9eb147
Igor <Igor@HOME-PC>
2023-05-14 18:41:06 +1000

    Задачи 70 и 2466

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using LeetCode.Array;
+using LeetCode.Dynamic_Programming;
 using LeetCode.Matrix;
 using LeetCode.String;
 using System;
@@ -9,17 +10,8 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            var nums1 = new int[8][];
-            nums1[0] = new int[] { 21, 5 };
-            nums1[1] = new int[] { 92, 3 };
-            nums1[2] = new int[] { 74, 2 };
-            nums1[3] = new int[] { 39, 4 };
-            nums1[4] = new int[] { 58, 2 };
-            nums1[5] = new int[] { 5, 5 };
-            nums1[6] = new int[] { 49, 4 };
-            nums1[7] = new int[] { 65, 3 };
-
-            var result = ArraySolutions.MostPoints(nums1);
+            var result = DynamicProgramming.CountGoodStrings(9, 9, 4, 2);
+        
             Console.WriteLine(result);
         }
     }
