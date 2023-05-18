d2ae5dca5b70ee10ce114481035c9f6f0ed5c8b6
Igor <Igor@HOME-PC>
2023-05-18 20:54:58 +1000

    1558

---
+++ LeetCode/1500-1599.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class _1500_1599
+    {
+        /// <summary>
+        /// 1558. Minimum Numbers of Function Calls to Make Target Array. Graph
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="edges"></param>
+        /// <returns></returns>
+        public static IList<int> FindSmallestSetOfVertices(int n, IList<IList<int>> edges)
+        {
+            if (n <= 1)
+            {
+                return new List<int>();
+            }
+
+            var result = new List<int>();
+
+            var seconds = edges.Select(edge => edge.ElementAt(1)).Distinct();
+
+            var empty = Enumerable.Range(0, n).Except(seconds).ToList();
+
+            return empty;
+        }
+    }
+}
