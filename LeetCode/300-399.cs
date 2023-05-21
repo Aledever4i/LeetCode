daf5adbfdb4b45db0a65d26c052bbe2ac8f0c5de
Igor <Igor@HOME-PC>
2023-05-22 01:04:30 +1000

    Задача 399

---
+++ LeetCode/300-399.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class _300_399
+    {
+        /// <summary>
+        /// 383. Ransom Note. Tags: Hash Table, String, Counting
+        /// </summary>
+        /// <param name="ransomNote"></param>
+        /// <param name="magazine"></param>
+        /// <returns></returns>
+        public static bool CanConstruct(string ransomNote, string magazine)
+        {
+            var ransomList = ransomNote.ToCharArray().ToList();
+
+            foreach (char i in magazine)
+            {
+                ransomList.Remove(i);
+            }
+
+            return !ransomList.Any();
+        }
+
+        public class NodeConnect
+        {
+            public string To;
+
+            public double Cost;
+        }
+
+        /// <summary>
+        /// 399. Evaluate Division. Tags. Array, Depth-First Search, Breadth-First Search, Union Find, Graph, Shortest Path
+        /// </summary>
+        public static double[] CalcEquation(IList<IList<string>> equations, double[] values, IList<IList<string>> queries)
+        {
+            var connects = new Dictionary<string, List<NodeConnect>>();
+            var result = new double[queries.Count()];
+
+            for (int i = 0; i < equations.Count(); i++)
+            {
+                if (!connects.TryAdd(equations[i][0], new List<NodeConnect>() { new NodeConnect() { To = equations[i][1], Cost = values[i] } }))
+                {
+                    connects[equations[i][0]].Add(new NodeConnect() { To = equations[i][1], Cost = values[i] });
+                }
+
+                if (!connects.TryAdd(equations[i][1], new List<NodeConnect>() { new NodeConnect() { To = equations[i][0], Cost = 1 / values[i] } }))
+                {
+                    connects[equations[i][1]].Add(new NodeConnect() { To = equations[i][0], Cost = 1 / values[i] });
+                }
+            }
+
+            for (int i = 0; i < queries.Count(); i++)
+            {
+                var startPoint = queries[i][0];
+                var endPoint = queries[i][1];
+                var visited = new List<string>() { startPoint };
+
+                if (!connects.ContainsKey(startPoint))
+                {
+                    result[i] = -1.0;
+                }
+                else if (startPoint.Equals(endPoint))
+                {
+                    result[i] = 1.0;
+                }
+                else
+                {
+                    var cost = FindNode(visited, 1.0, startPoint, endPoint);
+
+                    if (cost == 0.0)
+                    {
+                        result[i] = -1.0;
+                    }
+                    else
+                    {
+                        result[i] = cost;
+                    }
+                }
+            }
+
+            return result;
+
+            double FindNode(List<string> visited, double cost, string from, string to)
+            {
+                var ways = connects[from];
+                double result = cost;
+                visited.Add(from);
+
+                var found = ways.Where(way => way.To == to).FirstOrDefault();
+
+                if (found != null)
+                {
+                    result *= found.Cost;
+                }
+                else if (ways.Any(w => !visited.Contains(w.To)))
+                {
+                    result *=
+                        ways
+                            .Where(w => !visited.Contains(w.To))
+                            .Select(
+                                w =>
+                                {
+                                    return FindNode(visited, w.Cost, w.To, to);
+                                }
+                            )
+                            .Max();
+                }
+                else
+                {
+                    result *= 0;
+                }
+
+                return result;
+            }
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -11,24 +11,41 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            var array = new int[] { 0, 0, 0, 1, 1, 2, 4, 9};
-            ListNode end = new ListNode(array[array.Length - 1], null);
-            ListNode head = null;
-            for (int i = array.Length - 2; i >= 0; i--)
-            {
-                head = new ListNode(array[i], end);
-                end = head;
-            }
+            //var array = new int[] { 0, 0, 0, 1, 1, 2, 4, 9};
+            //ListNode end = new ListNode(array[array.Length - 1], null);
+            //ListNode head = null;
+            //for (int i = array.Length - 2; i >= 0; i--)
+            //{
+            //    head = new ListNode(array[i], end);
+            //    end = head;
+            //}
 
             //var list = new List<IList<int>>() { new List<int>() { 1, 3 }, new List<int>() { 2, 0 }, new List<int>() { 2, 3 }, new List<int>() { 1, 0 }, new List<int>() { 4, 1 }, new List<int>() { 0, 3 } };
 
             //var array = new int[10] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 } ;
 
+
             // [[4,1],[0,2],[1,3,4],[2,4],[0,3,2]] false
             // [[4,1],[0,2],[1,3],[2,4],[0,3]] true
             // [[1,4],[0,2],[1,4,3],[2],[0,2]] true
 
-            var result = _1_99.DeleteDuplicates(head);
+            IList<IList<string>> equations = new List<IList<string>>() { 
+                new List<string>() { "x1", "x2" },
+                new List<string>() { "x2", "x3" },
+                new List<string>() { "x3", "x4" },
+                new List<string>() { "x4", "x5" },
+            };
+            double[] values = new double[] { 3.0, 4.0, 5.0, 6.0 };
+            IList<IList<string>> queries = new List<IList<string>>() { 
+                new List<string>() { "x1", "x5" },
+                new List<string>() { "x5", "x2" },
+                new List<string>() { "x2", "x4" },
+                new List<string>() { "x2", "x2" },
+                new List<string>() { "x2", "x9" },
+                new List<string>() { "x9", "x9" }
+            };
+
+            var result = _300_399.CalcEquation(equations, values, queries);
         
             Console.WriteLine(result);
         }
