e5c1920a14cb137faa4b4b9b6c5ac1f109ca4385
Igor <Igor@HOME-PC>
2023-06-01 23:34:04 +1000

    Задача 1091

---
+++ LeetCode/100-199.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class _100_199
+    {
+        /// <summary>
+        /// 118. Pascal's Triangle. Tags: Array, Dynamic Programming
+        /// </summary>
+        public static IList<IList<int>> Generate(int numRows)
+        {
+            var result = new List<IList<int>>();
+
+            result.Add(new List<int>() { 1 });
+
+            if (numRows == 1)
+            {
+                return result;
+            }
+
+            result.Add(new List<int>() { 1, 1 });
+
+            if (numRows == 2)
+            {
+                return result;
+            }
+
+            for (int i = 2; i < numRows; i++)
+            {
+                var values = new List<int>();
+
+                for (int j = 0; j < i + 1; j++)
+                {
+                    if (j == 0 || j == i)
+                    {
+                        values.Add(1);
+                    }
+                    else
+                    {
+                        values.Add(result[i - 1][j - 1] + result[i - 1][j]);
+                    }
+                }
+
+                result.Add(values);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 119. Pascal's Triangle II. Tags: Array, Dynamic Programming
+        /// </summary>
+        public static IList<int> GenerateRow(int rowIndex)
+        {
+            var result = new List<IList<int>>();
+
+            result.Add(new List<int>() { 1 });
+
+            if (rowIndex == 0)
+            {
+                return result.ElementAt(rowIndex);
+            }
+
+            result.Add(new List<int>() { 1, 1 });
+
+            if (rowIndex == 1)
+            {
+                return result.ElementAt(rowIndex);
+            }
+
+            for (int i = 2; i < rowIndex + 1; i++)
+            {
+                var values = new List<int>();
+
+                for (int j = 0; j < i + 1; j++)
+                {
+                    if (j == 0 || j == i)
+                    {
+                        values.Add(1);
+                    }
+                    else
+                    {
+                        values.Add(result[i - 1][j - 1] + result[i - 1][j]);
+                    }
+                }
+
+                result.Add(values);
+            }
+
+            return result.ElementAt(rowIndex);
+        }
+
+        /// <summary>
+        /// 121. Best Time to Buy and Sell Stock. Tags: Array, Dynamic Programming
+        /// </summary>
+        /// [7,1,5,3,6,4]
+        /// [7,6,4,3,1]
+        public static int MaxProfit1(int[] prices)
+        {
+            var n = prices.Length;
+            if (n == 1)
+            {
+                return 0;
+            }
+
+            int[][] values = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                var price = prices[i];
+
+                values[i] = new int[] { price };
+            }
+            return 0;
+        }
+    }
+}
---
+++ LeetCode/1000-1099.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class _1000_1099
+    {
+        /// <summary>
+        /// 1091. Shortest Path in Binary Matrix. Tags: Array, Breadth-First Search, Matrix
+        /// </summary>
+        public static int ShortestPathBinaryMatrix(int[][] grid)
+        {
+            var n = grid.Length;
+            var ways = new List<(int, int, int)>();
+            var results = new List<int>();
+
+            var visited = new int[n][];
+            for (var i = 0; i < n; i++ )
+            {
+                visited[i] = new int[n];
+            }
+
+            if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
+            {
+                return -1;
+            }
+
+            analyse(0, 0, 1);
+
+            while (ways.Count > 0)
+            {
+                var item = ways.First();
+                ways.RemoveAt(0);
+
+                analyse(item.Item1, item.Item2, item.Item3);
+            }
+
+            return results.Any() ? results.Min() : -1;
+
+            void analyse(int x, int y, int count)
+            {
+                if (x == n - 1 && y == n - 1)
+                {
+                    results.Add(count);
+                    return;
+                }
+
+                if (x < n - 1 && y < n - 1 && visited[x + 1][y + 1] < 8 && grid[x + 1][y + 1] == 0)
+                {
+                    ways.Add((x + 1, y + 1, count + 1));
+                    visited[x + 1][y + 1] += 1;
+                }
+
+                if (x < n - 1 && visited[x + 1][y] < 8 && grid[x + 1][y] == 0)
+                {
+                    ways.Add((x + 1, y, count + 1));
+                    visited[x + 1][y] += 1;
+                }
+
+                if (y < n - 1 && visited[x][y + 1] < 8 && grid[x][y + 1] == 0)
+                {
+                    ways.Add((x, y + 1, count + 1));
+                    visited[x][y + 1] += 1;
+                }
+
+                if (x > 0 && y < n - 1 && visited[x - 1][y + 1] < 8 && grid[x - 1][y + 1] == 0)
+                {
+                    ways.Add((x - 1, y + 1, count + 1));
+                    visited[x - 1][y + 1] += 1;
+                }
+
+                if (y > 0 && visited[x][y - 1] < 8 && grid[x][y - 1] == 0)
+                {
+                    ways.Add((x, y - 1, count + 1));
+                    visited[x][y - 1] += 1;
+                }
+
+                if (y > 0 && x < n - 1 && visited[x + 1][y - 1] < 8 && grid[x + 1][y - 1] == 0)
+                {
+                    ways.Add((x + 1, y - 1, count + 1));
+                    visited[x + 1][y - 1] += 1;
+                }
+
+                if (x > 0 && visited[x - 1][y] < 8 && grid[x - 1][y] == 0)
+                {
+                    ways.Add((x - 1, y, count + 1));
+                    visited[x - 1][y] += 1;
+                }
+
+                if (x > 0 && y > 0 && visited[x - 1][y - 1] < 8 && grid[x - 1][y - 1] == 0)
+                {
+                    ways.Add((x - 1, y - 1, count + 1));
+                    visited[x - 1][y - 1] += 1;
+                }
+            }
+        }
+
+    }
+}
