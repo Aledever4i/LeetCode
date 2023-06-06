04ea41cd017ea8ee472b594c673e2397a86611c2
Igor <Igor@HOME-PC>
2023-06-06 23:31:09 +1000

    1502

--- LeetCode/1500-1599.cs
+++ LeetCode/1500-1599.cs
@@ -10,6 +10,31 @@ namespace LeetCode
     public static class _1500_1599
     {
         /// <summary>
+        /// 1502. Can Make Arithmetic Progression From Sequence. Tags: Array, Sorting
+        /// </summary>
+        public static bool CanMakeArithmeticProgression(int[] arr)
+        {
+            if (arr.Length == 2)
+            {
+                return true;
+            }
+
+            var sorted = arr.OrderBy(value => value);
+            var firstElement = sorted.ElementAt(0);
+            var step = sorted.ElementAt(1) - firstElement;
+
+            for (int i = 2; i < arr.Length; i++)
+            {
+                if (sorted.ElementAt(i) != firstElement + step * i)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
         /// 1558. Minimum Numbers of Function Calls to Make Target Array. Graph
         /// </summary>
         /// <param name="n"></param>
