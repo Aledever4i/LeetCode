7e383a782f2a75b44f8d7628df80a5b6fb0c37f2
Igor <Igor@HOME-PC>
2023-06-14 11:48:07 +1000

    530

--- LeetCode/500-599.cs
+++ LeetCode/500-599.cs
@@ -9,6 +9,72 @@ namespace LeetCode
     public static class _500_599
     {
         /// <summary>
+        /// 530. Minimum Absolute Difference in BST. Tags: Tree, Depth-First Search, Breadth-First Search, Binary Search Tree, Binary Tree,
+        /// </summary>
+        public static int GetMinimumDifference(TreeNode root)
+        {
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            var array = new List<int>();
+            var result = int.MaxValue;
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                var value = node.val;
+
+                var index = array.BinarySearch(value);
+                if (index < 0)
+                {
+                    index = ~index;
+                }
+                array.Insert(index, value);
+
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+
+                if (array.Count() == 2)
+                {
+                    result = Math.Abs(array[0] - array[1]);
+                }
+                else if (array.Count() > 2)
+                {
+                    if (index > 0)
+                    {
+                        var tempResult = Math.Abs(array[index] - array[index - 1]);
+                        result = (result > tempResult) ? tempResult : result;
+
+                        if (result == 0)
+                        {
+                            return result;
+                        }
+                    }
+
+                    if (array.Count - 1 != index)
+                    {
+                        var tempResult = Math.Abs(array[index] - nextValue);
+                        result = (result > tempResult) ? tempResult : result;
+
+                        if (result == 0)
+                        {
+                            return result;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
         /// 547. Number of Provinces. Tags: Depth-First Search, Breadth-First Search, Union Find, Graph
         /// </summary>
         public static int FindCircleNum(int[][] isConnected)
