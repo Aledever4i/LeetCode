19f564346908c97b9d2250d62c83b0f4e29449a2
Игорь <@HOME-PC>
2023-07-12 01:02:06 +1000

    863

--- LeetCode/800-899.cs
+++ LeetCode/800-899.cs
@@ -63,84 +63,150 @@ namespace LeetCode
         }
 
         /// <summary>
+        /// 863. All Nodes Distance K in Binary Tree
+        /// TODO: НЕ РЕШИЛ САМ
+        /// </summary>
+        public static IList<int> DistanceK(TreeNode root, TreeNode target, int k)
+        {
+            var graph = new Dictionary<int, List<int>>();
+            dfsBuild(root, null, graph);
+
+            var answer = new List<int>();
+            HashSet<int> visited = new HashSet<int>();
+            Queue<int[]> queue = new Queue<int[]>();
+
+            queue.Enqueue(new int[] { target.val, 0 });
+            visited.Add(target.val);
+
+            while (queue.Count() > 0)
+            {
+                int[] cur = queue.Dequeue();
+                int node = cur[0], distance = cur[1];
+                if (distance == k)
+                {
+                    answer.Add(node);
+                    continue;
+                }
+
+                if (graph.TryGetValue(node, out List<int> value))
+                {
+                    foreach (var neighbor in value)
+                    {
+                        if (!visited.Contains(neighbor))
+                        {
+                            visited.Add(neighbor);
+                            queue.Enqueue(new int[] { neighbor, distance + 1 });
+                        }
+                    }
+                }
+            }
+
+            return answer;
+
+            void dfsBuild(TreeNode cur, TreeNode parent, Dictionary<int, List<int>> graph)
+            {
+                if (cur != null && parent != null)
+                {
+                    int curVal = cur.val, parentVal = parent.val;
+
+                    graph.TryAdd(curVal, new List<int>());
+                    graph.TryAdd(parentVal, new List<int>());
+
+                    graph[curVal].Add(parentVal);
+                    graph[parentVal].Add(curVal);
+                }
+
+                if (cur != null && cur.left != null)
+                {
+                    dfsBuild(cur.left, cur, graph);
+                }
+
+                if (cur != null && cur.right != null)
+                {
+                    dfsBuild(cur.right, cur, graph);
+                }
+            }
+        }        
+
+        /// <summary>
         /// 877. Stone Game. Tags: Array, Math, Dynamic Programming, Game Theory
         /// </summary>
         public static bool StoneGame(int[] piles)
-        {
-            int count = piles.Length;
-
-            var dp = new int[count + 2][];
-            for (int y = 0; y < count + 2; y++)
             {
-                dp[y] = new int[count + 2];
-            }
+                int count = piles.Length;
 
-            var first = 0;
-            var second = 0;
-            var step = 0;
-
-            for (int x = 0, y = count - 1; step < count; step++)
-            {
-                var who = (x + y % 2);
-                var left1 = piles[x] + GetMAxPoint(x + 2, y);
-                var right = piles[y] + GetMAxPoint(x, y - 2);
-
-                if (left1 > right)
+                var dp = new int[count + 2][];
+                for (int y = 0; y < count + 2; y++)
                 {
-                    if (who == 1)
-                    {
-                        first += piles[x];
-                    }
-                    else
-                    {
-                        second += piles[x];
-                    }
-                    x = x + 1;
-                }
-                else
-                {
-                    if (who == 1)
-                    {
-                        first += piles[y];
-                    }
-                    else
-                    {
-                        second += piles[y];
-                    }
-                    y = y - 1;
-                }
-            }
-
-            return first > second;
-
-            int GetMAxPoint(int x, int y)
-            {
-                if (x > y || y < x || x < 0 || y < 0)
-                {
-                    return 0;
+                    dp[y] = new int[count + 2];
                 }
 
-                if (dp[x][y] > 0)
-                {
-                    return dp[x][y];
-                }
+                var first = 0;
+                var second = 0;
+                var step = 0;
 
-                if (x == y || Math.Abs(x - y) == 1)
+                for (int x = 0, y = count - 1; step < count; step++)
                 {
-                    dp[x][y] = Math.Max(piles[x], piles[y]);
-
-                    return dp[x][y];
-                }
-                else
-                {
+                    var who = (x + y % 2);
                     var left1 = piles[x] + GetMAxPoint(x + 2, y);
-                    var right2 = piles[y] + GetMAxPoint(x, y - 2);
-                    
-                    dp[x][y] = Math.Max(left1, right2);
+                    var right = piles[y] + GetMAxPoint(x, y - 2);
+
+                    if (left1 > right)
+                    {
+                        if (who == 1)
+                        {
+                            first += piles[x];
+                        }
+                        else
+                        {
+                            second += piles[x];
+                        }
+                        x = x + 1;
+                    }
+                    else
+                    {
+                        if (who == 1)
+                        {
+                            first += piles[y];
+                        }
+                        else
+                        {
+                            second += piles[y];
+                        }
+                        y = y - 1;
+                    }
                 }
 
-                return dp[x][y];
+                return first > second;
+
+                int GetMAxPoint(int x, int y)
+                {
+                    if (x > y || y < x || x < 0 || y < 0)
+                    {
+                        return 0;
+                    }
+
+                    if (dp[x][y] > 0)
+                    {
+                        return dp[x][y];
+                    }
+
+                    if (x == y || Math.Abs(x - y) == 1)
+                    {
+                        dp[x][y] = Math.Max(piles[x], piles[y]);
+
+                        return dp[x][y];
+                    }
+                    else
+                    {
+                        var left1 = piles[x] + GetMAxPoint(x + 2, y);
+                        var right2 = piles[y] + GetMAxPoint(x, y - 2);
+                    
+                        dp[x][y] = Math.Max(left1, right2);
+                    }
+
+                    return dp[x][y];
+                }
             }
-        }
     }
 }
