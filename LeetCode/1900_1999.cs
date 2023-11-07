32aba44da67fa055e2fe60b064b1ce7c5bc6df51
Игорь <@HOME-PC>
2023-11-07 11:48:57 +1000

    1921

--- LeetCode/1900_1999.cs
+++ LeetCode/1900_1999.cs
@@ -12,32 +12,21 @@ namespace LeetCode
             var result = 0;
             var n = dist.Length;
 
-            var union = new (int, int)[n];
-            for (var i = 0; i < n; i++)
-            {
-                union[i] = (dist[i], speed[i]);
-            }
-
-            Array.Sort(union, new Comparison<(int, int)>((a, b) => a.Item1.CompareTo(b.Item2)));
-
             var time = new double[n];
             for (int i = 0; i < n; i++)
             {
-                time[i] = (1.0 * union[i].Item1) / (1.0 * union[i].Item2);
+                time[i] = (1.0 * dist[i]) / (1.0 * speed[i]);
             }
 
             Array.Sort(time);
 
             for (int i = 0; i < n; i++)
             {
-                if (i < time[i])
-                {
-                    result++;
-                }
-                else
+                if (time[i] <= i)
                 {
                     break;
                 }
+                result++;
             }
 
             return result;
