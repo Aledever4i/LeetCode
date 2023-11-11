9879755ccf4e8d302577831909d121be42200948
Игорь <@HOME-PC>
2023-11-12 01:29:40 +1000

    2642

---
+++ LeetCode/2600-2699.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// 2642. Design Graph With Shortest Path Calculator
+    /// </summary>
+    public class Graph2642
+    {
+        private readonly List<(int Node, int Cost)>[] _graph;
+
+        public Graph2642(int n, int[][] edges)
+        {
+            _graph = new List<(int Node, int Cost)>[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                _graph[i] = new List<(int Node, int Cost)>();
+            }
+
+            foreach (var edge in edges)
+            {
+                AddEdge(edge);
+            }
+        }
+
+        public void AddEdge(int[] edge)
+        {
+            _graph[edge[0]].Add((edge[1], edge[2]));
+        }
+
+        public int ShortestPath(int node1, int node2)
+        {
+            if (node1 == node2)
+            {
+                return 0;
+            }
+            else if (!_graph[node1].Any())
+            {
+                return -1;
+            }
+
+            HashSet<int> visited = new HashSet<int>();
+            var queue = new PriorityQueue<int, int>();
+            queue.Enqueue(node1, 0);
+
+
+            while (queue.TryDequeue(out int currNode, out int currCost))
+            {
+                if (visited.Contains(currNode))
+                {
+                    continue;
+                }
+
+                if (currNode == node2)
+                { 
+                    return currCost;
+                }
+
+                visited.Add(currNode);
+
+                foreach (var next in _graph[currNode])
+                {
+                    queue.Enqueue(next.Node, next.Cost + currCost);
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    public static class _2600_2699
+    {
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -10,10 +10,73 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            var result = _1700_1799.RestoreArray(new int[3][] { new int[2] { 2, 1 }, new int[2] { 3, 4 }, new int[2] { 3, 2 } });
+            //var result = new Graph2642(4, new int[27][] {
+            //    new int[] {11,6,84715 },
+            //    new int[] { 7,9,764823},
+            //    new int[] { 6,0,315591},
+            //    new int[] { 1,4,909432},
+            //    new int[] { 6,5,514907},
+            //    new int[] { 9,6,105610},
+            //    new int[] {3,10,471042 },
+            //    new int[] {7,10,348752 },
+            //    new int[] {5,11,715628 },
+            //    new int[] { 6,1,973999},
+            //    new int[] { 8,7,593929},
+            //    new int[] {7,6,64688 },
+            //    new int[] { 6,4,741734},
+            //    new int[] { 10,1,894247},
+            //    new int[] { 9,7,81181},
+            //    new int[] { 2,11,75418},
+            //    new int[] {12,2,85431 },
+            //    new int[] { 7,2,260306},
+            //    new int[] {11,9,640614 },
+            //    new int[] {2,3,648804 },
+            //    new int[] { 4,12,568023},
+            //    new int[] { 0,8,730096},
+            //    new int[] {9,11,633474 },
+            //    new int[] {3,6,390214 },
+            //    new int[] { 1,10,117955},
+            //    new int[] {9,8,222602 },
+            //    new int[] { 10,7,689294}
+            //});
+
+            //result.AddEdge(new int[] { 1, 2, 36450 });
+            //result.AddEdge(new int[] { 8, 0, 709628 });
+            //result.AddEdge(new int[] { 2, 4, 30185 });
+            //result.AddEdge(new int[] { 12, 1, 21696 });
+            //result.AddEdge(new int[] { 1, 8, 2553 });
+            //result.AddEdge(new int[] { 4, 6, 2182 });
+            //result.AddEdge(new int[] { 7, 5, 206 });
+            //result.AddEdge(new int[] { 5, 7, 140 });
+            //result.AddEdge(new int[] { 12, 6, 365184 });
+            //result.AddEdge(new int[] { 3, 2, 5 });
+            //result.AddEdge(new int[] { 0, 11, 5 });
+            //result.AddEdge(new int[] { 1, 7, 5 });
+
+            var result = new Graph2642(1, new int[0][] { });
+            var a = result.ShortestPath(0, 0);
+            Console.WriteLine(a);
 
 
-            Console.WriteLine(result);
+
+            //var a = result.ShortestPath(3, 2);
+            //a = result.ShortestPath(0, 3);
+            //var a = result.ShortestPath(11, 11);
+            Console.WriteLine(a); 
+            
+            a = result.ShortestPath(7, 4);
+            Console.WriteLine(a);
+
+            a = result.ShortestPath(0, 12);
+            Console.WriteLine(a);
+
+            //a = result.ShortestPath(11, 11);
+            //Console.WriteLine(a);
+
+            //a = result.ShortestPath(11, 11);
+            //Console.WriteLine(a);
         }
     }
 }
+
+//[[]],[[]],[0,8],[],[7,4],[0,12],[[3,9,858944]],[[0,9,4]],[3,5],[4,5],[12,9],[9,8],[3,5],[[12,9,629052]],[3,8],[[4,0,545201]],[11,8],[4,11],[9,6],[[12,7,4]],[7,10],[2,5],[6,11],[12,2],[9,7],[[4,3,879736]],[1,3],[1,0],[4,6]]
