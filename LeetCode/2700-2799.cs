944fae3bada9386a42eb9e43fe813692b6096598
Игорь <@HOME-PC>
2024-01-01 14:34:30 +1000

    2706

---
+++ LeetCode/2700-2799.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class _2700_2799
+    {
+        /// <summary>
+        /// 2706. Buy Two Chocolates
+        /// </summary>
+        public static int BuyChoco(int[] prices, int money)
+        {
+            System.Array.Sort(prices);
+
+            var result = money - prices[0] - prices[1];
+
+            if (result >= 0)
+            {
+                return result;
+            }
+
+            return money;
+        }
+    }
+}
