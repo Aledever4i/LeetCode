28dc47740279cfc86a05334401e1002c6e206005
Игорь <@HOME-PC>
2024-01-14 12:20:18 +1000

    3000, 3001, 3002

---
+++ LeetCode/3000-3099.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Contest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class _3000_3099
+    {
+        /// <summary>
+        /// 3000. Maximum Area of Longest Diagonal Rectangle
+        /// </summary>
+        public static int AreaOfMaxDiagonal(int[][] dimensions)
+        {
+            return _379.AreaOfMaxDiagonal(dimensions);
+        }
+
+        /// <summary>
+        /// 3001. Minimum Moves to Capture The Queen
+        /// </summary>
+        public static int MinMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f)
+        {
+            return _379.MinMovesToCaptureTheQueen(a, b, c, d, e, f);
+        }
+
+        /// <summary>
+        /// 3002. Maximum Size of a Set After Removals
+        /// </summary>
+        public static int MaximumSetSize(int[] nums1, int[] nums2)
+        {
+            return _379.MaximumSetSize(nums1, nums2);
+        }
+    }
+}
