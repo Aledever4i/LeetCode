697d2fa45eda39e1ceb2318557d6e4ed630218a3
Игорь <@HOME-PC>
2024-02-15 23:17:01 +1000

    3036

--- LeetCode/3000-3099.cs
+++ LeetCode/3000-3099.cs
@@ -1,6 +1,7 @@
 ﻿using LeetCode.Contest.Transfered;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +32,94 @@ namespace LeetCode
         public static int MaximumSetSize(int[] nums1, int[] nums2)
         {
             return _379.MaximumSetSize(nums1, nums2);
+        }
+
+        /// <summary>
+        /// 3036. Number of Subarrays That Match a Pattern II
+        /// </summary>
+        public static int CountMatchingSubarrays(int[] nums, int[] pattern)
+        {
+            int n = nums.Length;
+            int p = pattern.Length;
+
+            var val = new int[n];
+            for (var i = 0; i < n - 1; i++)
+            {
+                if (nums[i] == nums[i + 1]) val[i] = 0;
+                if (nums[i] > nums[i + 1]) val[i] = -1;
+                if (nums[i] < nums[i + 1]) val[i] = 1;
+            }
+            val[n - 1] = -5;
+
+            return KMPSearch(pattern, val);
+
+            int KMPSearch(int[] pattern, int[] txt)
+            {
+                int pLen = pattern.Length;
+                int tLen = txt.Length;
+                int matchCount = 0;
+
+                var lps = computeLPSArray(pattern);
+
+                int tIdx = 0, pIdx = 0;
+                while (tIdx < tLen)
+                {
+                    if (pattern[pIdx] == txt[tIdx])
+                    {
+                        pIdx++; tIdx++;
+                    }
+
+                    if (pIdx == pLen)
+                    {
+                        matchCount++;
+                        pIdx = lps[pIdx - 1];
+                    }
+                    else if (tIdx < tLen && pattern[pIdx] != txt[tIdx])
+                    {
+                        if (pIdx != 0)
+                        {
+                            pIdx = lps[pIdx - 1];
+                        }
+                        else
+                        {
+                            tIdx++; 
+                        } 
+                    }
+                }
+
+                return matchCount;
+            }
+
+            int[] computeLPSArray(int[] pat)
+            {
+                var pLen = pat.Length;
+                var lps = new int[pLen];
+                lps[0] = 0;
+
+                int len = 0;
+                int i = 1;
+                while (i < pLen)
+                {
+                    if (pat[i] == pat[len])
+                    {
+                        lps[i] = ++len;
+                        i++;
+                    }
+                    else
+                    {
+                        if (len != 0)
+                        {
+                            len = lps[len - 1];
+                        }
+                        else
+                        {
+                            lps[i++] = len;
+                        }
+                    }
+                }
+
+                return lps;
+            }
         }
     }
 }
