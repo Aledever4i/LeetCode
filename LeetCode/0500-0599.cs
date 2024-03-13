12ebee41509fb0cebc4c6ec436b45f2d810e55d1
Igor Sukhov <igorsuchov@yandex.ru>
2024-03-13 20:19:19 +1000

    561

--- LeetCode/0500-0599.cs
+++ LeetCode/0500-0599.cs
@@ -288,5 +288,21 @@ namespace LeetCode
             var reversedWords = words.Select(word => string.Join("", word.Reverse()));
             return string.Join(" ", reversedWords);
         }
+
+        /// <summary>
+        /// 561. Array Partition
+        /// </summary>
+        public static int ArrayPairSum(int[] nums)
+        {
+            System.Array.Sort(nums);
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i += 2)
+            {
+                sum += Math.Min(nums[i], nums[i + 1]);
+            }
+
+            return sum;
+        }
     }
 }
