656ce994c57173e877653eb7e8b38e164d3c5a19
Igor Sukhov <igorsuchov@yandex.ru>
2024-03-14 23:18:56 +1000

    930

--- LeetCode/0900-0999.cs
+++ LeetCode/0900-0999.cs
@@ -119,6 +119,40 @@ namespace LeetCode
                 return totalCount;
             }
         }
+        public static int NumSubarraysWithSum2(int[] nums, int goal)
+        {
+            int start = 0;
+            int prefixZeros = 0;
+            int currentSum = 0;
+            int totalCount = 0;
+
+            for (int end = 0; end < nums.Length; end++)
+            {
+                currentSum += nums[end];
+
+                while (start < end && (nums[start] == 0 || currentSum > goal))
+                {
+                    if (nums[start] == 1)
+                    {
+                        prefixZeros = 0;
+                        currentSum -= 1;
+                    }
+                    else
+                    {
+                        prefixZeros++;
+                    }
+
+                    start++;
+                }
+
+                if (currentSum == goal)
+                {
+                    totalCount += 1 + prefixZeros;
+                }
+            }
+
+            return totalCount;
+        }
 
         /// <summary>
         /// 931. Minimum Falling Path Sum
