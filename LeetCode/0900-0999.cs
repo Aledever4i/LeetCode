cfb5196f84b15ae6bbe93842be6c4b5eeae20078
Igor Sukhov <igorsuchov@yandex.ru>
2024-03-14 22:59:43 +1000

    930

--- LeetCode/0900-0999.cs
+++ LeetCode/0900-0999.cs
@@ -95,6 +95,30 @@ namespace LeetCode
             return (int)answer;
         }
 
+        /// <summary>
+        /// 930. Binary Subarrays With Sum
+        /// </summary>
+        public static int NumSubarraysWithSum(int[] nums, int goal)
+        {
+            return slidingWindowAtMost(nums, goal) - slidingWindowAtMost(nums, goal - 1);
+
+            int slidingWindowAtMost(int[] nums, int goal)
+            {
+                int start = 0, currentSum = 0, totalCount = 0;
+
+                for (int end = 0; end < nums.Length; end++)
+                {
+                    currentSum += nums[end];
+                    while (start <= end && currentSum > goal)
+                    {
+                        currentSum -= nums[start++];
+                    }
+                    totalCount += end - start + 1;
+                }
+
+                return totalCount;
+            }
+        }
 
         /// <summary>
         /// 931. Minimum Falling Path Sum
