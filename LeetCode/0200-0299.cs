be78548a5b19f6eb99463b460b243169c10e0548
Igor Sukhov <igorsuchov@yandex.ru>
2024-09-20 00:40:58 +1000

    241

--- LeetCode/0200-0299.cs
+++ LeetCode/0200-0299.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Numerics;
@@ -491,6 +492,49 @@ namespace LeetCode
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// 241. Different Ways to Add Parentheses
+        /// </summary>
+        public static IList<int> DiffWaysToCompute(string expression)
+        {
+            var res = new List<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char oper = expression[i];
+                if (oper == '+' || oper == '-' || oper == '*')
+                {
+                    var s1 = DiffWaysToCompute(expression[..i]);
+                    var s2 = DiffWaysToCompute(expression[(i + 1)..]);
+                    foreach (int a in s1)
+                    {
+                        foreach (int b in s2)
+                        {
+                            if (oper == '+')
+                            {
+                                res.Add(a + b);
+                            }
+                            else if (oper == '-')
+                            {
+                                res.Add(a - b);
+                            }
+                            else if (oper == '*')
+                            { 
+                                res.Add(a * b);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (res.Count == 0)
+            {
+                res.Add(Int32.Parse(expression));
+            }
+
+            return res;
         }
 
         /// <summary>
