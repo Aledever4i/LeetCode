912f3043a1e7e7c3638a540f061553b22073c1c8
Igor Sukhov <igorsuchov@yandex.ru>
2024-11-04 12:58:38 +1000

    3163

---
+++ LeetCode/3100-3199.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _3100_3199
+    {
+        /// <summary>
+        /// 3163. String Compression III
+        /// </summary>
+        public string CompressedString(string word)
+        {
+            var builder = new StringBuilder();
+            var currentNumber = 0;
+            var currentChar = '-';
+
+            foreach (char c in word)
+            {
+                if (c == currentChar && currentNumber < 9)
+                {
+                    currentNumber++;
+                    continue;
+                }
+
+                builder.Append(currentNumber);
+                builder.Append(currentChar);
+
+                currentNumber = 1;
+                currentChar = c;
+            }
+
+            builder.Append(currentNumber);
+            builder.Append(currentChar);
+
+            return builder.ToString().Remove(0, 2);
+        }
+    }
+}
