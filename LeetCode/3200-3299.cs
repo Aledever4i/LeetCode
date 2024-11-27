26707b1cbbc43a790deb153a95baf716e7c20402
Igor Sukhov <igorsuchov@yandex.ru>
2024-11-27 23:33:00 +1000

    3243

--- LeetCode/3200-3299.cs
+++ LeetCode/3200-3299.cs
@@ -1,49 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LeetCode
 {
     public static class _3200_3299
     {
         /// <summary>
-        /// 3254. Find the Power of K-Size Subarrays I
+        /// 3243. Shortest Distance After Road Addition Queries I
         /// </summary>
-        public static int[] ResultsArray(int[] nums, int k)
+        public static int[] ShortestDistanceAfterQueries(int n, int[][] queries)
         {
-            if (k == 1)
+            List<int> answer = [];
+            List<List<int>> adjList = [];
+
+            for (int i = 0; i < n; i++)
             {
-                return nums;
+                adjList.Add([]);
             }
 
-            var result = new int[nums.Length - k + 1];
-
-            for (int i = 1; i < nums.Length; i++)
+            for (int i = 0; i < n - 1; i++)
             {
-                if (nums[i] - 1 == nums[i - 1])
+                adjList[i].Add(i + 1);
+            }
+
+            foreach (var road in queries)
+            {
+                int u = road[0];
+                int v = road[1];
+                adjList[u].Add(v);
+                answer.Add(bfs(n, adjList));
+            }
+
+            return [.. answer];
+
+            int bfs(int n, List<List<int>> adjList)
+            {
+                bool[] visited = new bool[n];
+                Queue<int> nodeQueue = [];
+                nodeQueue.Enqueue(0);
+                visited[0] = true;
+
+                int currentLayerNodeCount = 1;
+                int nextLayerNodeCount = 0;
+                int layersExplored = 0;
+
+                while (nodeQueue.Count > 0)
                 {
-                    if (i - k + 1 >= 0 && result[i - k + 1] != -1)
+                    for (int i = 0; i < currentLayerNodeCount; i++)
                     {
-                        result[i - k + 1] = nums[i];
-                    }
-                }
-                else
-                {
-                    var iter = 1;
-                    while (iter < k)
-                    {
-                        if (i - iter < nums.Length - k + 1 && i - iter >= 0)
+                        int currentNode = nodeQueue.Dequeue();
+
+                        if (currentNode == n - 1)
                         {
-                            result[i - iter] = -1;
+                            return layersExplored;
                         }
-                        iter++;
-                    }
-                }
-            }
 
-            return result;
+                        foreach (var neighbor in adjList[currentNode])
+                        {
+                            if (visited[neighbor]) continue;
+
+                            nodeQueue.Enqueue(neighbor);
+                            nextLayerNodeCount++;
+                            visited[neighbor] = true;
+                        }
+                    }
+
+                    currentLayerNodeCount = nextLayerNodeCount;
+                    nextLayerNodeCount = 0;
+                    layersExplored++;
+                }
+
+                return -1;
+            }
         }
 
         /// <summary>
---
+++ LeetCode/Algorithms/DijkstraAlgorithm.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Algorithms
+{
+    public static class DijkstraAlgorithm
+    {
+        public static void FindShortestPath(
+            Dictionary<string, Dictionary<string, int>> graph,
+            Dictionary<string, int> costs,
+            Dictionary<string, string?> parents
+        )
+        {
+            var visited = new HashSet<string>();
+            var lowestCostUnvisitedNode = FindLowestCostUnvisitedNode(costs, visited);
+            while (lowestCostUnvisitedNode != null)
+            {
+                var cost = costs[lowestCostUnvisitedNode];
+                var neighbors = graph[lowestCostUnvisitedNode];
+
+                foreach (var neighbor in neighbors)
+                {
+                    var newCost = cost + neighbor.Value;
+                    if (newCost < costs[neighbor.Key])
+                    {
+                        costs[neighbor.Key] = newCost;
+                        parents[neighbor.Key] = lowestCostUnvisitedNode;
+                    }
+                }
+
+                visited.Add(lowestCostUnvisitedNode);
+                lowestCostUnvisitedNode = FindLowestCostUnvisitedNode(costs, visited);
+            }
+        }
+
+        private static string FindLowestCostUnvisitedNode(Dictionary<string, int> costs, HashSet<string> visited)
+        {
+            var cost = costs
+                .Where(node => !visited.Contains(node.Key))
+                .OrderBy(node => node.Value)
+                .FirstOrDefault();
+
+            return cost.Key;
+        }
+
+        public static string ConstructFinalRoute(Dictionary<string, string?> parents)
+        {
+            var route = new List<string>();
+            var currentNode = "Finish";
+
+            while (currentNode != null)
+            {
+                route.Add(currentNode);
+                currentNode = parents.GetValueOrDefault(currentNode);
+            }
+
+            route.Reverse();
+            return string.Join("=>", route);
+        }
+    }
+}
