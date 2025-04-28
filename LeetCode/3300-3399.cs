41fc2086f3278bb654c32e79e0174b9113fbeedd
Igor Sukhov <igorsuchov@yandex.ru>
2025-04-28 11:34:27 +1000

    3392

--- LeetCode/3300-3399.cs
+++ LeetCode/3300-3399.cs
@@ -4,162 +4,179 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LeetCode
+namespace LeetCode;
+
+public static class _3300_3399
 {
-    public static class _3300_3399
+    /// <summary>
+    /// 3304. Find the K-th Character in String Game I
+    /// </summary>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public static char KthCharacter(int k)
     {
-        /// <summary>
-        /// 3304. Find the K-th Character in String Game I
-        /// </summary>
-        /// <param name="k"></param>
-        /// <returns></returns>
-        public static char KthCharacter(int k)
+        var s = "abbcbccdbccdcddebccdcddecddedeefbccdcddecddedeefcddedeefdeefeffgbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhibccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhicddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhideefeffgeffgfggheffgfgghfgghghhieffgfgghfgghghhifgghghhighhihiij"; //generate();
+
+        return s[k-1];
+
+        char nextChar(char a)
         {
-            var s = "abbcbccdbccdcddebccdcddecddedeefbccdcddecddedeefcddedeefdeefeffgbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhibccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhicddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhideefeffgeffgfggheffgfgghfgghghhieffgfgghfgghghhifgghghhighhihiij"; //generate();
-
-            return s[k-1];
-
-            char nextChar(char a)
+            if (a == 'z')
             {
-                if (a == 'z')
-                {
-                    return 'a';
-                }
-
-                return (char)((int)a + 1);
+                return 'a';
             }
 
-            string generate()
-            {
-                var result = new List<char>() { 'a' };
-
-                while (result.Count <= 500)
-                {
-                    var temp = new List<char>();
-
-                    foreach (var c in result)
-                    {
-                        temp.Add(nextChar(c));
-                    }
-
-                    result.AddRange(temp);
-                }
-
-                return string.Join("", result);
-            }
+            return (char)((int)a + 1);
         }
 
-
-        /// <summary>
-        /// 3305. Count of Substrings Containing Every Vowel and K Consonants I
-        /// </summary>
-        public static int CountOfSubstrings(string word, int k)
+        string generate()
         {
-            var dict = new Dictionary<char, int>
-            {
-                { 'a', 0 },
-                { 'e', 0 },
-                { 'i', 0 },
-                { 'o', 0 },
-                { 'u', 0 }
-            };
+            var result = new List<char>() { 'a' };
 
-            var other = 0;
-            var multi = 1;
-            var start = 0;
-            var end = 0;
-            var result = 0;
-
-            while (start < word.Length - 4)
+            while (result.Count <= 500)
             {
-                while ((dict['a'] == 0 || dict['e'] == 0 || dict['i'] == 0 || dict['o'] == 0 || dict['u'] == 0) || other < k)
+                var temp = new List<char>();
+
+                foreach (var c in result)
                 {
-                    if (end == word.Length)
-                    {
-                        break;
-                    }
+                    temp.Add(nextChar(c));
+                }
 
-                    if (dict.ContainsKey(word[end]))
-                    {
-                        dict[word[end]]++;
-                    }
-                    else
-                    {
-                        other++;
-                    }
+                result.AddRange(temp);
+            }
+
+            return string.Join("", result);
+        }
+    }
+
+
+    /// <summary>
+    /// 3305. Count of Substrings Containing Every Vowel and K Consonants I
+    /// </summary>
+    public static int CountOfSubstrings(string word, int k)
+    {
+        var dict = new Dictionary<char, int>
+        {
+            { 'a', 0 },
+            { 'e', 0 },
+            { 'i', 0 },
+            { 'o', 0 },
+            { 'u', 0 }
+        };
+
+        var other = 0;
+        var multi = 1;
+        var start = 0;
+        var end = 0;
+        var result = 0;
+
+        while (start < word.Length - 4)
+        {
+            while ((dict['a'] == 0 || dict['e'] == 0 || dict['i'] == 0 || dict['o'] == 0 || dict['u'] == 0) || other < k)
+            {
+                if (end == word.Length)
+                {
+                    break;
+                }
+
+                if (dict.ContainsKey(word[end]))
+                {
+                    dict[word[end]]++;
+                }
+                else
+                {
+                    other++;
+                }
+                end++;
+            }
+
+            while (other > k)
+            {
+                if (dict.ContainsKey(word[start]))
+                {
+                    dict[word[start]]--;
+                }
+                else
+                {
+                    other--;
+                }
+                start++;
+                multi = 1;
+            }
+
+            while (other == k && dict['a'] > 0 && dict['e'] > 0 && dict['i'] > 0 && dict['o'] > 0 && dict['u'] > 0)
+            {
+                while (end < word.Length && dict.ContainsKey(word[end]))
+                {
+                    dict[word[end]]++;
+                    multi++;
                     end++;
                 }
 
-                while (other > k)
+                result += multi;
+
+                while (start < word.Length && dict.ContainsKey(word[start]))
                 {
-                    if (dict.ContainsKey(word[start]))
-                    {
-                        dict[word[start]]--;
-                    }
-                    else
-                    {
-                        other--;
-                    }
-                    start++;
-                    multi = 1;
-                }
+                    dict[word[start]]--;
 
-                while (other == k && dict['a'] > 0 && dict['e'] > 0 && dict['i'] > 0 && dict['o'] > 0 && dict['u'] > 0)
-                {
-                    while (end < word.Length && dict.ContainsKey(word[end]))
+                    if (dict[word[start]] > 0)
                     {
-                        dict[word[end]]++;
-                        multi++;
-                        end++;
-                    }
-
-                    result += multi;
-
-                    while (start < word.Length && dict.ContainsKey(word[start]))
-                    {
-                        dict[word[start]]--;
-
-                        if (dict[word[start]] > 0)
+                        if (word[start] == word[end - 1])
                         {
-                            if (word[start] == word[end - 1])
-                            {
-                                result += 1;
-                            }
-                            else
-                            {
-                                result += multi;
-                            }
-
-                            start++;
+                            result += 1;
                         }
                         else
                         {
-
-                            start++;
-                            break;
+                            result += multi;
                         }
-                    }
 
-                    if (start < word.Length && !dict.ContainsKey(word[start]))
-                    {
-                        multi = 1;
-                        other--;
                         start++;
                     }
+                    else
+                    {
+
+                        start++;
+                        break;
+                    }
                 }
 
-                if ((dict['a'] == 0 || dict['e'] == 0 || dict['i'] == 0 || dict['o'] == 0 || dict['u'] == 0) && end >= word.Length)
+                if (start < word.Length && !dict.ContainsKey(word[start]))
                 {
-                    return result;
-                }
-
-                if (dict['a'] > 0 && dict['e'] > 0 && dict['i'] > 0 && dict['o'] > 0 && dict['u'] > 0 && other < k && end >= word.Length)
-                {
-                    return result;
+                    multi = 1;
+                    other--;
+                    start++;
                 }
             }
 
-            return result;
+            if ((dict['a'] == 0 || dict['e'] == 0 || dict['i'] == 0 || dict['o'] == 0 || dict['u'] == 0) && end >= word.Length)
+            {
+                return result;
+            }
+
+            if (dict['a'] > 0 && dict['e'] > 0 && dict['i'] > 0 && dict['o'] > 0 && dict['u'] > 0 && other < k && end >= word.Length)
+            {
+                return result;
+            }
         }
+
+        return result;
+    }
+
+    /// <summary>
+    /// 3392. Count Subarrays of Length Three With a Condition
+    /// </summary>
+    public static int CountSubarrays(int[] nums)
+    {
+        var result = 0;
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            if ((nums[i] + nums[i + 2]) * 2.0 == nums[i + 1])
+            {
+                result++;
+            }
+        }
+
+        return result;
     }
 }
